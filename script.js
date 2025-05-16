7c67d0ba360c9ac4c83449db4a6e3661b702be6f
samarmalla <samar.malla1@gmail.com>
2025-05-15 20:13:34 -0700

    objects

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let students = {
+    name: "John",
+    marks : 75
+}
+
+if (students.marks >= 50){
+    console.log(students.name+ " has passed.")
+}else{
+    console.log(students.name+ " has failed.")
+}
+
+console.log(students.name+ " has scored " + students.marks + " marks.");
+
+//hands-on2
+for(let i=1; i<100; i++){
+    if(i%2==0){
+        console.log(i + " is even");
+    }
+}
 
